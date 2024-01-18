3237fde2de682ab4ed8b52d988848be271bb71bc
nikitka <nikitakitsunedev@gmail.com>
2024-01-18 22:37:55 +0300

    version 1

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,26 @@
-// @todo: Темплейт карточки
+// @todo: Темплейт карточки ✓
 
-// @todo: DOM узлы
+// @todo: DOM узлы ✓
 
-// @todo: Функция создания карточки
+// @todo: Функция создания карточки ✓
 
 // @todo: Функция удаления карточки
 
-// @todo: Вывести карточки на страницу
+// @todo: Вывести карточки на страницу ✓
+
+
+function createCard(name, link) {
+    const cardsList = document.querySelector('.places__list');
+    const cardTemplate = document.querySelector('#card-template').content;
+    const card = cardTemplate.cloneNode(true);
+
+    card.querySelector('.card__title').textContent = name;
+    card.querySelector('.card__image').alt = name;
+    card.querySelector('.card__image').src = link;
+
+    cardsList.append(card)
+}
+
+initialCards.forEach(function (item) { 
+    createCard(item.name, item.link)    
+})
