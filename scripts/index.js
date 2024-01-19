fc559878ca14d8dbb8c6feef845364f1044132f5
nikitka <nikitakitsunedev@gmail.com>
2024-01-19 16:09:01 +0300

    release v2

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,8 @@
+const cardsList = document.querySelector('.places__list');
+
 // Ф-ия удаления карточки мест
 const deleteCard = (evt) => {
-    const cardsList = document.querySelector('.places__list');
-    const card = evt.target.parentElement
+    const card = evt.target.closest('.places__item.card')
     cardsList.removeChild(card)
 }
 
@@ -9,10 +10,11 @@ const deleteCard = (evt) => {
 function createCard(name, link, deleteCard) {
     const cardTemplate = document.querySelector('#card-template').content;
     const card = cardTemplate.cloneNode(true);
+    const cardImage = card.querySelector('.card__image')
 
     card.querySelector('.card__title').textContent = name;
-    card.querySelector('.card__image').alt = name;
-    card.querySelector('.card__image').src = link;
+    cardImage.alt = name;
+    cardImage.src = link;
     card.querySelector('.card__delete-button').addEventListener('click', (evt) => deleteCard(evt))
 
     return card
@@ -20,6 +22,5 @@ function createCard(name, link, deleteCard) {
 
 // Создание карточек из массива 
 initialCards.forEach(function (item) { 
-    const cardsList = document.querySelector('.places__list');
     cardsList.append(createCard(item.name, item.link, deleteCard))    
 })
