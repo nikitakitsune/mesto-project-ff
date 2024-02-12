48f5eb83ab842d64f32ab5cade3943234b1957da
nikitka <nikitakitsunedev@gmail.com>
2024-02-12 14:27:50 +0300

    add like functional

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,10 @@ import { initialCards, createCard, deleteCard } from "./cards.js";
 import { openModal, closeModal } from "./modal.js";
 // Parent element for cards
 const cardsList = document.querySelector(".places__list");
-// Modal buttons
+// Modals buttons
 const editButton = document.querySelector(".profile__edit-button");
-const newCardButton = document.querySelector(".profile__add-button")
-const closeButton = document.querySelector(".popup__close");
-// Modal elements
-const popup = document.querySelector(".popuop");
+const newCardButton = document.querySelector(".profile__add-button");
+// Modals elements
 const editModal = document.querySelector(".popup_type_edit");
 const newCardModal = document.querySelector(".popup_type_new-card");
 const imageModal = document.querySelector(".popup_type_image");
@@ -23,7 +21,27 @@ editButton.addEventListener("click", function () {
   openModal(editModal);
 });
 
-newCardButton.addEventListener("click" function () {
+newCardButton.addEventListener("click", function () {
   openModal(newCardModal);
-})
-// Close modals
+});
+
+// Close modals function
+const addCloseEvent = (el) => {
+  el.addEventListener("click", (evt) => {
+    const { classList: target } = evt.target;
+    if (target.contains("popup__close") || target.contains("popup")) {
+      closeModal(el);
+    }
+  });
+};
+
+// Adding a close function to each modals
+[editModal, newCardModal, imageModal].forEach(addCloseEvent);
+
+// Likes functional
+cardsList.addEventListener("click", function (evt) {
+  const target = evt.target.classList
+  if (target.contains("card__like-button")) {
+    target.add('card__like-button_is-active')
+  }
+});
